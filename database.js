28dfd73a76ca039753a6d9883c7cd6c6c44550a4
Projet Tech Academy <mathilde.martine.paisley@gmail.com>
2025-11-30 11:04:11 +0100

    Add files via upload

---
+++ database.js
@@ -0,0 +1,24 @@
+// ======================================================================
+// 🧠 BASE DE DONNÉES CENTRALE (MMPA🌹)
+// Copie tes liens Google Drive ici. Ils s'afficheront partout.
+// ======================================================================
+
+const dbRessources = {
+    // EXEMPLE : La clé doit être le nom exact du fichier (ou le code)
+    "B0_M01_S001": { 
+        audio: "https://lien_vers_https://drive.google.com/file/d/1YXvlY1gZThr7tmNMCNs4zV_stpAsp7wm/view?usp=drive_linkaudio",
+        video: "httphttps://drive.google.com/file/d/1v-SkshpxUUQbRa-kpRWX71rME3ZkIo5t/view?usp=drive_links://lien_vers_video",
+        mindmap: "https://drive.google.com/file/d/1_OcOsCuAw_gazFfIvMJsmgYDBkheSMYN/view?usp=drive_link",
+        report: "https://lien_vers_rapport",
+        learn: "https://lien_vers_fiche",
+        quiz: "https://lien_vers_quiz",
+        info: "https://lien_vhttps://drive.google.com/file/d/1_jIUvYOrpshFHRC_aWuXCybVZDqTQiwc/view?usp=drive_linkers_infographie",
+        pdf: "htthttps://drive.google.com/file/d/1E6Hk9kVz6mCEgr5vWKL0xv9JGUQEbv42/view?usp=drive_linkps://lien_vers_pdf_officiel", // Si vide, cherche le fichier local
+        studi: "httpshttps://app.comptalia.com/v3/medialibrary/lib/resources?promotionId=4104&subjectId=49238&moduleId=70012&themeId=107321://www.studi.com/fr/connexion"
+    },
+    
+    // Ajoute les autres ici...
+    "B0_M01_S002": {
+        // ...
+    }
+};
