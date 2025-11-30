fdb8f9a932ad8f8d4096fdf9202db5d48e09cdec
Projet Tech Academy <mathilde.martine.paisley@gmail.com>
2025-11-30 11:39:41 +0100

    Add files via upload

--- database.js
+++ database.js
@@ -1,24 +1,22 @@
 // ======================================================================
 // 🧠 BASE DE DONNÉES CENTRALE (MMPA🌹)
-// Copie tes liens Google Drive ici. Ils s'afficheront partout.
 // ======================================================================
 
 const dbRessources = {
-    // EXEMPLE : La clé doit être le nom exact du fichier (ou le code)
-    "B0_M01_S001": { 
-        audio: "https://lien_vers_https://drive.google.com/file/d/1YXvlY1gZThr7tmNMCNs4zV_stpAsp7wm/view?usp=drive_linkaudio",
-        video: "httphttps://drive.google.com/file/d/1v-SkshpxUUQbRa-kpRWX71rME3ZkIo5t/view?usp=drive_links://lien_vers_video",
+    // NOTE IMPORTANTE : La clé doit correspondre exactement au nom du fichier HTML de la session.
+    // Pour "B0_M01_S001 La notion de droit du travail", la clé est :
+    "B0_M01_S001_La_notion_de_droit_du_travail": { 
+        audio: "https://drive.google.com/file/d/1YXvlY1gZThr7tmNMCNs4zV_stpAsp7wm/view?usp=drive_link",
+        video: "https://drive.google.com/file/d/1v-SkshpxUUQbRa-kpRWX71rME3ZkIo5t/view?usp=drive_link",
         mindmap: "https://drive.google.com/file/d/1_OcOsCuAw_gazFfIvMJsmgYDBkheSMYN/view?usp=drive_link",
-        report: "https://lien_vers_rapport",
-        learn: "https://lien_vers_fiche",
-        quiz: "https://lien_vers_quiz",
-        info: "https://lien_vhttps://drive.google.com/file/d/1_jIUvYOrpshFHRC_aWuXCybVZDqTQiwc/view?usp=drive_linkers_infographie",
-        pdf: "htthttps://drive.google.com/file/d/1E6Hk9kVz6mCEgr5vWKL0xv9JGUQEbv42/view?usp=drive_linkps://lien_vers_pdf_officiel", // Si vide, cherche le fichier local
-        studi: "httpshttps://app.comptalia.com/v3/medialibrary/lib/resources?promotionId=4104&subjectId=49238&moduleId=70012&themeId=107321://www.studi.com/fr/connexion"
+        info: "https://drive.google.com/file/d/1_jIUvYOrpshFHRC_aWuXCybVZDqTQiwc/view?usp=drive_link",
+        pdf: "https://drive.google.com/file/d/1E6Hk9kVz6mCEgr5vWKL0xv9JGUQEbv42/view?usp=drive_link",
+        studi: "https://app.comptalia.com/v3/medialibrary/lib/resources?promotionId=4104&subjectId=49238&moduleId=70012&themeId=107321",
+        // Ajoutez les autres types si vous avez des liens (sinon laissez vide ou supprimez la ligne)
+        report: "",
+        learn: "",
+        quiz: ""
     },
     
-    // Ajoute les autres ici...
-    "B0_M01_S002": {
-        // ...
-    }
+    // Pour ajouter le suivant, copiez le bloc ci-dessus et changez la clé (ex: B0_M01_S002...)
 };
